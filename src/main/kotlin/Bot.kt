40a8149ba7f515fb959900295061f2458f7773c6
Kopy <56215553+Kopymatic@users.noreply.github.com>
2022-07-31 15:57:01 -0600

    refactor Bot.kt

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
-import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.requests.GatewayIntent
 import kotlin.system.exitProcess
 
 val log = Reference.log
@@ -23,80 +23,79 @@ fun main(args: Array<String>) {
         exitProcess(1)
     }
     Reference.connection = Reference.connect(args[1], args[2], args[3])
-    Bot.init(args[0])
-}
 
-class Bot : ListenerAdapter() {
-    companion object {
-        fun init(token: String) {
-            log.info("Creating JDA instance...")
-            Reference.jda = light(token, enableCoroutines = true).awaitReady()
+    log.info("""
+                --Configuration--
+                    Experimental: ${Reference.experimental}
+                    Version: ${Reference.version}
+                    ${if(Reference.experimental) Reference.debugGuild else ""}
+            """.trimIndent())
+    log.info("Creating JDA instance...")
+    Reference.jda = light(args[0], enableCoroutines = true, builder = {this.enableIntents(GatewayIntent.MESSAGE_CONTENT)}).awaitReady()
 
-            log.debug("Getting commands...")
-            val commands = AllCommands.commands
+    log.debug("Getting commands...")
+    val commands = AllCommands.commands
 
-            log.info("Registering message listener...")
-            Reference.jda.listener<MessageReceivedEvent> {
-                var hasPrefix = false
-                var name: String? = null
-                for (prefix in Reference.prefixes) {
-                    if (it.message.contentRaw.startsWith(prefix)) {
-                        hasPrefix = true
-                        name = it.message.contentRaw.substring(prefix.length).split(" ")[0]
-                        break
-                    }
-                }
-                if (!hasPrefix) return@listener
-                if (name == null) return@listener
-
-                for (command in commands) {
-                    if (command.supportsText && (command.textCommandData.name.lowercase() == name.lowercase()) || (command.textCommandData.aliases != null && command.textCommandData.aliases!!.contains(name.lowercase()))) {
-                        command.textCommandReceived(it)
-                        break
-                    }
-                }
+    log.info("Registering message listener...")
+    Reference.jda.listener<MessageReceivedEvent> {
+        var hasPrefix = false
+        var name: String? = null
+        for (prefix in Reference.prefixes) {
+            if (it.message.contentRaw.startsWith(prefix)) {
+                hasPrefix = true
+                name = it.message.contentRaw.substring(prefix.length).split(" ")[0]
+                break
             }
+        }
+        if (!hasPrefix) return@listener
+        if (name == null) return@listener
 
-            log.info("Registering slash listener...")
-            Reference.jda.listener<SlashCommandInteractionEvent> {
-                it.deferReply().await()
-                for (command in commands) {
-                    if (command.supportsSlash && command.slashCommandData.name == it.name) {
-                        command.slashCommandReceived(it)
-                        break
-                    }
-                }
+        for (command in commands) {
+            if (command.supportsText && (command.textCommandData.name.lowercase() == name.lowercase()) || (command.textCommandData.aliases != null && command.textCommandData.aliases!!.contains(name.lowercase()))) {
+                command.textCommandReceived(it)
+                break
             }
-
-            log.info("Registering autocomplete listener...")
-            Reference.jda.listener<CommandAutoCompleteInteractionEvent> {
-                for (command in commands) {
-                    if (command.slashCommandData.name == it.name) {
-                        command.onAutoComplete(it)
-                        break
-                    }
-                }
-            }
-
-            log.info("Registering slash commands...")
-            if (Reference.experimental) {
-                val guild = Reference.jda.getGuildById(Reference.debugGuild)!!
-                for (command in commands) {
-                    if (command.supportsSlash) {
-                        log.info("Registering guild command ${command.slashCommandData.name}...")
-                        guild.upsertCommand(command.slashCommandData).queue()
-                    }
-                }
-            } else {
-                for (command in commands) {
-                    if (command.supportsSlash) {
-                        log.info("Registering command ${command.slashCommandData.name}...")
-                        Reference.jda.upsertCommand(command.slashCommandData).queue()
-                    }
-                }
-            }
-
-            Reference.jda.presence.activity = Activity.watching("Version ${Reference.version} ${if (Reference.experimental) "Experimental" else ""}")
         }
     }
+
+    log.info("Registering slash listener...")
+    Reference.jda.listener<SlashCommandInteractionEvent> {
+        it.deferReply().await()
+        for (command in commands) {
+            if (command.supportsSlash && command.slashCommandData.name == it.name) {
+                command.slashCommandReceived(it)
+                break
+            }
+        }
+    }
+
+    log.info("Registering autocomplete listener...")
+    Reference.jda.listener<CommandAutoCompleteInteractionEvent> {
+        for (command in commands) {
+            if (command.slashCommandData.name == it.name) {
+                command.onAutoComplete(it)
+                break
+            }
+        }
+    }
+
+    log.info("Registering slash commands...")
+    if (Reference.experimental) {
+        val guild = Reference.jda.getGuildById(Reference.debugGuild)!!
+        for (command in commands) {
+            if (command.supportsSlash) {
+                log.info("Registering guild command ${command.slashCommandData.name}...")
+                guild.upsertCommand(command.slashCommandData).queue()
+            }
+        }
+    } else {
+        for (command in commands) {
+            if (command.supportsSlash) {
+                log.info("Registering command ${command.slashCommandData.name}...")
+                Reference.jda.upsertCommand(command.slashCommandData).queue()
+            }
+        }
+    }
+
+    Reference.jda.presence.activity = Activity.watching("Version ${Reference.version} ${if (Reference.experimental) "Experimental" else ""}")
 }
